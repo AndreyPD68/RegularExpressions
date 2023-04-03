4803aee478783caf67f1d7bc547e51680d7c6de0
Андрей <Reddevil992@list.ru>
2023-04-03 10:46:19 +0500

    1

--- main.py
+++ main.py
@@ -1,142 +1,48 @@
-import psycopg2
+from pprint import pprint
+## Читаем адресную книгу в формате CSV в список contacts_list:
+import csv
+import re
+
+pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+substitutuion = r'+7(\2)-\3-\4-\5 \6\7'
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+pprint(contacts_list)
+def main(contact_list: list):
+    new_list = list()
+    for item in contact_list:
+        full_name = ' '.join(item[:3]).split(' ')
+        result = [full_name[0], full_name[1], full_name[2], item[3], item[4],
+                  re.sub(pattern, substitutuion, item[5]),
+                  item[6]]
+        new_list.append(result)
+    return union(new_list)
 
 
+def union(contacts: list):
+    for contact in contacts:
+        first_name = contact[0]
+        last_name = contact[1]
+        for new_contact in contacts:
+            new_first_name = new_contact[0]
+            new_last_name = new_contact[1]
+            if first_name == new_first_name and last_name == new_last_name:
+                if contact[2] == "": contact[2] = new_contact[2]
+                if contact[3] == "": contact[3] = new_contact[3]
+                if contact[4] == "": contact[4] = new_contact[4]
+                if contact[5] == "": contact[5] = new_contact[5]
+                if contact[6] == "": contact[6] = new_contact[6]
 
-# удаление таблиц для новой работы
-def delete_tables(cur):
-    cur.execute("""
-    DROP TABLE IF EXISTS Client_phones""")
-    cur.execute("""
-    DROP TABLE  IF EXISTS Clients""")
-    conn.commit()
+    result_list = list()
+    for i in contacts:
+        if i not in result_list:
+            result_list.append(i)
 
-# Создаю таблицы
-def create_db(cur):
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Clients(  
-        client_id INTEGER PRIMARY KEY,
-        last_name VARCHAR(40) ,
-        first_name VARCHAR(40),
-        email VARCHAR(60)
-    );
-    """)
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS Client_phones(  
-            phone_number  VARCHAR PRIMARY KEY,
-            client_id INTEGER REFERENCES Clients(client_id)
-        );
-        """)
-    conn.commit()
-
-# Добавляю данные о клиенте
-def add_client(cur, client_id, first_name, last_name, email, *phones):
-    cur.execute("""
-        INSERT INTO Clients (client_id, last_name, first_name, email) VALUES(%s, %s, %s,%s)
-        RETURNING email, last_name, first_name;
-            """, (client_id,  last_name, first_name, email))
-    print(f'Клиент №{client_id} - , {cur.fetchall()}')
-
-    if phones:
-        add_phone(cur, client_id, *phones)
-
-
-def add_phone(cur, client_id, *phones):
-    for phone in phones:
-        cur.execute("""
-            INSERT INTO Client_phones (phone_number, client_id) VALUES(%s, %s) 
-            RETURNING phone_number, client_id;
-            """, (phone, client_id))
-        print(f'Номер Телефона клиента с id: {client_id} - {cur.fetchall()}')
-
-# смена данных
-def change_client_data(cur, client_id, first_name=None, last_name=None, email=None,
-                       new_phones=None, old_phones=None):
-    print('Обновляю данные')
-    if first_name:
-        cur.execute("""
-                    UPDATE Clients SET first_name=%s WHERE client_id=%s;""", (first_name, client_id))
-        conn.commit()
-    if last_name:
-        cur.execute("""
-                    UPDATE Clients SET first_name=%s WHERE client_id=%s;""", (last_name, client_id))
-        conn.commit()
-    if email:
-        cur.execute("""
-                    UPDATE Clients SET first_name=%s WHERE client_id=%s;""", (email, client_id))
-        conn.commit()
-    cur.execute("""
-                    SELECT last_name, first_name, email FROM Clients WHERE client_id=%s
-                    ;
-                    """, (client_id,))
-
-    print(f'Смена данных id: {client_id}, новые данные {cur.fetchall()}')
-    if old_phones:
-        for old_phone in old_phones:
-            print('Удаление старых номеров телефона')
-            delete_phone(cur, old_phone)
-
-    if new_phones:
-        print('Добавление новых номеров телефона')
-        add_phone(cur, client_id, *new_phones)
-
-def delete_phone(cur, phone_number):
-    cur.execute("""
-        DELETE FROM Client_phones WHERE phone_number=%s
-        RETURNING phone_number, client_id;
-        """, (phone_number,))
-    print(f'Удаление телефона №{phone_number}  - {cur.fetchone()}')
-
-
-def delele_client(cur, id):
-    cur.execute("""
-        DELETE FROM Client_phones WHERE client_id=%s 
-        RETURNING client_id, phone_number;
-        """, (id,))
-    print(f'Удаление данных клиента №{id} - {cur.fetchall()}')
-
-    cur.execute("""
-        DELETE FROM Clients WHERE client_id=%s 
-        ;
-        """, (id,))
-    conn.commit()
-    print(f'Удаление всех данных клиента №{id}')
-
-# Поиск клиента по данным
-def find_client(cur, client_id=None,  first_name=None, last_name=None, email=None, phone_number=None):
-    if phone_number:
-        cur.execute("""
-                SELECT last_name FROM Clients 
-                JOIN Client_phones ON clients.client_id=client_phones.client_id
-                WHERE client_phones.phone_number=%s
-                ;
-                """, (phone_number,))
-        print(f'Поиск клиента по номеру телефона {phone_number} - {cur.fetchall()}')
-
-    else:
-        cur.execute("""
-            SELECT last_name FROM Clients 
-            WHERE client_id=%s OR first_name=%s OR last_name=%s OR email=%s
-            ;
-            """, (client_id,  first_name, last_name, email))
-        print(f'Поиск клиента по другим параметрам  - {cur.fetchall()}')
-
-
-with psycopg2.connect(database="Home_Work", user="postgres", password="***") as conn:
-    with conn.cursor() as cur:
-        delete_tables(cur)
-        create_db(cur)
-        add_client(cur, 1, 'Иван', 'Иванов', 'Ivanvano@mail.ru', '+79227776666', '+11233211496')
-        add_client(cur, 2, 'Геннадий', 'Букин', 'genabuk@gmail.com')
-        add_client(cur, 3, 'Олег', 'Шматко', 'Corporal@yandex.ru', '+777-777')
-        add_phone(cur, 2, '+71112223344')
-        change_client_data(cur, client_id=3, first_name='Олег', old_phones=['+777-777'],
-                           new_phones=['+666-666', '+888-888'])
-        delete_phone(cur, '+777-777')
-        delele_client(cur, 1)
-        find_client(cur, last_name='Букин')
-        find_client(cur, first_name='Иван') # клиент удален
-        find_client(cur, phone_number='+71112223344')
-        find_client(cur, phone_number='+777-777')  # номер удален
-        find_client(cur, client_id=3)
-
-conn.close()
+    return result_list
+## 2. Сохраните получившиеся данные в другой файл.
+## Код для записи файла в формате CSV:
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(main(contacts_list))
